73826de077a8c7ac7d738d4ea140da5c80ae473b
Dominic Sisneros <dsisnero@gmail.com>
2025-02-03 18:32:24 -0700

    refactor: Improve EditApiKeyCommand with error handling and base command inheritance

--- src/gmaps/commands/edit_api_key_command.cr
+++ src/gmaps/commands/edit_api_key_command.cr
@@ -1,22 +1,44 @@
+require "./base_command"
 @[ACONA::AsCommand("edit_api_key", description: "Edit the API key")]
-class Gmaps::EditApiKeyCommand < ACON::Command
+class Gmaps::EditApiKeyCommand < Gmaps::BaseCommand
   property config_loader : ConfigLoader = ConfigLoader.new
-  
-  protected def configure
-    self
-      .argument("gmaps_api_key", ACON::Input::Argument::Mode::REQUIRED, "API key")
+
+  def configure : Nil
+    argument("api_key", mode: :required, description: "Gmaps api key")
   end
+
+  def interact(input : ACON::Input::Interface, output : ACON::Output::Interface)
+     style = create_style(input,output)
+     if input.has_argument?("api_key")
+       style.puts "Api key given"
+     else
+       style.puts "No Api key given"
+     raise Gmaps::NoApiKeyError.new unless input.has_argument?("api_key")
+    end
+
+        
+  end
+      
 
   protected def execute(input : ACON::Input::Interface, output : ACON::Output::Interface) : ACON::Command::Status
-    api_key = input.argument("gmaps_api_key", String)
-    @config_loader.edit_key("GMAPS_API_KEY", api_key)
-    output.puts "API key updated in #{@config_loader.config_file}"
-    ACON::Command::Status::SUCCESS
-  end
-end
+    style = create_style(input,output)
+    begin
+    input.validate
+    rescue ex      
+       style.not_nil! ex.not_nil!.message
+        ACON::Command::Status::FAILURE
+    end
+    style = create_style(input, output)
+    api_key = input.argument("api_key", String)
+      @config_loader.edit_key("GMAPS_API_KEY", api_key)
+      style.puts "API key updated in #{@config_loader.config_file}"
+      ACON::Command::Status::SUCCESS
+    rescue ex : NoApiKeyError
+        style.not_nil! "need to provide argument 'api_key'"
+        ACON::Command::Status::FAILURE
+    rescue ex
+       style.not_nil! ex.not_nil!.message
+        ACON::Command::Status::FAILURE
+    end
 
-#   config = Gmaps::Config.from_yaml(config_file_path)
-#   config.api_key = input.get_argument("api_key").as_s
-#   config.save
-# else
-#   config = Gmaps::Config.new
+end
